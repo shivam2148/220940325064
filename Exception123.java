e4a2585dcc2f1807d915447dd8e7a2634590a38b
shivam2148 <118419432+shivam2148@users.noreply.github.com>
2022-11-18 11:07:20 +0530

    Add files via upload

---
+++ Exception123.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+class Arraylist
+{
+	public static void main(String args[])
+	{
+		ArrayList<String> a=new ArrayList<String>();
+		a.add("rohan");
+		a.add("mohan");
+		a.add("sohan");
+		a.add("bohan");
+		System.out.println(a);
+		for(String name:a)
+		{
+			System.out.println(a);
+			System.out.println(", ");
+		}
+	}
+}
---
+++ shape123.java
@@ -0,0 +1,59 @@
+class shape123
+{
+	void draw()
+	{
+		System.out.println("drawing shape");
+	}
+	void erase()
+	{
+		System.out.println("erase shape");
+	}
+}
+	class Circle extends shape123
+	{
+		void draw();
+		{
+			System.out.println("drawing circle");
+		}
+		void erase()
+	   {
+		System.out.println("erase circle");
+	   }
+	}
+	   class Triangle extends shape123
+	{
+		void draw();
+		{
+			System.out.println("drawing triangle");
+		}
+		void erase()
+	   {
+		System.out.println("erase triangle");
+	   }
+	}
+	      class Square extends shape123
+	{
+		void draw();
+		{
+			System.out.println("drawing Square");
+		}
+		void erase()
+	   {
+		System.out.println("erase Square");
+	   }
+	}
+	   public class Run
+	   {
+		   public static void main(String args[])
+		   {
+			   shape123 c=new Circle();
+			    shape123 t=new Triangle();
+				 shape123 s=new Square();
+				 c.draw();
+				 c.erase();
+				 t.draw();
+				 t.erase();
+				 s.draw();
+				 s.erase();
+		   }
+	   }
